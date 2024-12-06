07ae87b822de5440a69ebf613b2d52512d8566d4
Luanda <luandabrito.ads@gmail.com>
2024-12-06 19:21:21 -0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+//ref() cria dados reativos, para acessar esses dados precisamos usar o .value
+import {ref} from "vue";
+//variavel inicial
+const numero = ref(0);
+//funcao para aumentar
+function aumentar(){ 
+    numero.value ++
+}
+
+//funcao para diminuir
+function diminuir(){
+    if(numero.value > 0){
+        numero.value --
+    }
+}
+
+</script>
+
+<template>
+    <h1>Contador</h1>
+    <h2>{{ numero }}</h2>
+    <button @click="aumentar">+</button>
+    <button @click="diminuir">-</button>
+
+</template>
+
+<style scoped lang="scss">
+
+</style>
